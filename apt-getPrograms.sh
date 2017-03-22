fc42b0d778cda40966c7f3643a7362924adc131d
Paul Mullins <noobie375@live.com>
2017-03-22 10:53:42 -0400

    Get rid of all the echos

--- apt-getPrograms.sh
+++ apt-getPrograms.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
-# declare STRING variable
-STRING="Hello World"
-echo "Updating"
 sudo apt-get update
-echo "Installing Chromium"
 sudo apt-get -y install chromium-browser
-echo "Installing VLC"
 sudo apt-get -y install vlc
-echo "installing dolphin"
 sudo apt-get -y install dolphin
-echo "installing git"
 sudo apt-get -y install git
-echo "installing vim"
 sudo apt-get -y install vim
-echo "installing curl"
 sudo apt-get -y install curl3
-echo "installing p7zip-full"
 sudo apt-get -y install p7zip-full
-echo "installing tmux"
 sudo apt-get -y install tmux
-echo "installing cu"
 sudo apt-get -y install cu
-echo "installing x11vnc"
 sudo apt-get -y install x11vnc
-echo "installing nmap"
 sudo apt-get -y install nmap
-echo "installing arduino"
 sudo apt-get -y install arduino
 sudo apt-get -y install redshift
 sudo apt-get -y install openvpn
