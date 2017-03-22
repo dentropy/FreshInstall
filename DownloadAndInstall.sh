1ba579a0c1a4973d37d6f0d77b5262452d592031
Paul Mullins <mullinat@users.noreply.github.com>
2017-03-22 09:35:35 -0400

    Create DownloadAndInstall.sh
    Just download for now

---
+++ DownloadAndInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+wget https://atom.io/download/deb
+wget https://go.skype.com/linux.deb
+wget https://go.skype.com/linux.deb
+wget https://download.teamviewer.com/download/teamviewer_i386.deb
+wget https://telegram.org/dl/desktop/linux
+wget https://riot.im/packages/debian/dists/xenial/main/binary-amd64/
+
