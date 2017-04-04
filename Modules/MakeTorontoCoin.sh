cf850ebe7113009bc6c44583314d68f565940c9c
Paul Mullins <noobie375@live.com>
2017-04-03 20:55:45 -0400

    Use wget instead of curl because we need the file to finish downloading before running anything else

--- Modules/MakeTorontoCoin.sh
+++ Modules/MakeTorontoCoin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Instructions Courtesy of https://github.com/ethereum/go-ethereum/wiki/Installing-Go#ubuntu-1404
-curl -O https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
 mkdir -p ~/go; echo "export GOPATH=$HOME/go" >> ~/.bashrc
 echo "export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin" >> ~/.bashrc
