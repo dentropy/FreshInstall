ee509508a21b8e4ea5cb445689d7f145f6ab58c9
Paul Mullins <mullinat@users.noreply.github.com>
2018-07-21 17:48:37 -0400

    Update DownloadAndInstall.sh
    Changed to download what I really want
    No more google chrome
    Steam is something I do not need on every linux install

--- DownloadAndInstall.sh
+++ DownloadAndInstall.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 sudo apt-get update
-wget https://download.teamviewer.com/download/teamviewer_i386.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-http://download.virtualbox.org/virtualbox/5.1.18/virtualbox-5.1_5.1.18-114002~Ubuntu~xenial_amd64.deb
-wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
-wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
-wget https://downloads.arduino.cc/arduino-1.8.5-linux64.tar.xz
+curl -O https://download.teamviewer.com/download/teamviewer_i386.deb
+curl -O http:://download.virtualbox.org/virtualbox/5.1.18/virtualbox-5.1_5.1.18-114002~Ubuntu~xenial_amd64.deb
+curl -O https://prerelease.keybase.io/keybase_amd64.deb
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 chmod +x *.sh
 #Install teamviewer
 sudo dpkg -i teamviewer_i386.deb
 sudo apt-get -y -f install
 sudo dpkg -i teamviewer_i386.deb
-#Install google chrome
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get -y -f install
-sudo dpkg -i google-chrome-stable_current_amd64.deb
 #Install virtualbox
 sudo dpkg -i virtualbox*
 sudo apt-get -y -f install
 sudo dpkg -i virtualbox*
-#Install steam
-sudo dpkg -i steam*
-sudo apt-get -y -f install
-sudo dpkg -i steam*
+#Keybase
+curl -O https://prerelease.keybase.io/keybase_amd64.deb
+sudo dpkg -i keybase_amd64.deb
+sudo apt-get install -f
+run_keybase
 #Install Anaconda
 ./Anaconda*
