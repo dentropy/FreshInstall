910137235ab90957b8283eaf103e5e6c1708dddf
Paul Mullins <mullinat@users.noreply.github.com>
2018-07-21 18:39:10 -0400

    Update DesktopInstall.sh
    Fixed bug install atom

--- DesktopInstall.sh
+++ DesktopInstall.sh
@@ -23,7 +23,7 @@ sudo apt-get install -y keepass2
 
 
 #Install everything else
-sudo snap install atom
+sudo snap install atom --classic
 sudo apt-get install shutter
 sudo apt -y install chromium-browser
 sudo snap install brave
