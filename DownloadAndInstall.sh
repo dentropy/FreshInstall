31bb5cc7c6804146d0bd036ea37acacf41d55b26
Paul Mullins <8509074+dentropy@users.noreply.github.com>
2018-08-26 21:15:01 -0400

    Update DownloadAndInstall.sh
    Configure PIA in network menu for Gnome

--- DownloadAndInstall.sh
+++ DownloadAndInstall.sh
@@ -2,7 +2,10 @@
 sudo apt-get update
 curl -O https://prerelease.keybase.io/keybase_amd64.deb
 curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+curl -o https://www.privateinternetaccess.com/installer/pia-nm.sh
 chmod +x *.sh
+#PIA
+sudo bash pia-nm.sh
 #Keybase
 curl -O https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
