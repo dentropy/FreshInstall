949ed671efc93ee3a6dee6720842bf7e0aef8489
Paul Mullins <8509074+dentropy@users.noreply.github.com>
2018-08-26 23:34:32 -0400

    Update ServerInstall.sh
    Adding docker installation to ServerInstall.sh

--- ServerInstall.sh
+++ ServerInstall.sh
@@ -20,7 +20,31 @@ sudo apt -y install nmon
 sudo apt -y install terminator
 sudo apt -y install youtube-dl
 sudo apt -y install htop
-#sudo apt -y install bsdgames
+sudo apt -y install bsdgames
+
+
+#Install docker from docker website
+#https://docs.docker.com/install/linux/docker-ce/ubuntu/#os-requirements
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get -y update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+#sudo docker run hello-world
+#https://docs.docker.com/compose/install/#install-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 
 #Install and configure thefuck
