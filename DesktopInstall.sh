3ea88ef442fd5726feace623950f8201e8d08fb2
Paul Mullins <8509074+dentropy@users.noreply.github.com>
2018-08-26 23:32:50 -0400

    Update DesktopInstall.sh
    Deleting docker install from Desktop install

--- DesktopInstall.sh
+++ DesktopInstall.sh
@@ -53,29 +53,6 @@ sudo apt -y install cu
 sudo apt -y install network-manager-openvpn-gnome
 sudo apt -y install shutter
 
-#Install docker from docker website
-#https://docs.docker.com/install/linux/docker-ce/ubuntu/#os-requirements
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce
-#sudo docker run hello-world
-#https://docs.docker.com/compose/install/#install-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-
 #Automate installing gnome night light slider later
 chromium-browser https://github.com/TimurKiyivinski/gnome-shell-night-light-slider-extension
 chromium-browser https://itsfoss.com/gnome-shell-extensions/
