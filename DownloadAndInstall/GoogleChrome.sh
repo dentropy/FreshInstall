b244f696b98ea7c9bc8754abaf1e7b413b4ba991
July312018Ubuntu18.04 <paulmullins@protonmail.com>
2018-08-31 20:21:33 -0400

    Did not actually fix the problem, now it is fixed

--- DownloadAndInstall/GoogleChrome.sh
+++ DownloadAndInstall/GoogleChrome.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-curl -o https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #Install google chrome
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -y -f install
--- DownloadAndInstall/Mega.sh
+++ DownloadAndInstall/Mega.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megasync-xUbuntu_18.04_amd64.deb
+curl -O https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megasync-xUbuntu_18.04_amd64.deb
 sudo dpkg -i megasync*
 sudo apt -y -f install
 sudo dpkg -i megasync*
