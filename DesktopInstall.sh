54334133eaa3ca70386a4577dc80ca634d1a125b
July312018Ubuntu18.04 <paulmullins@protonmail.com>
2018-08-31 20:13:48 -0400

    Added Download and Install of VSCode into its own file, using snap currently

--- DesktopInstall.sh
+++ DesktopInstall.sh
@@ -8,12 +8,6 @@ sudo add-apt-repository ppa:phoerious/keepassxc
 
 
 sudo apt -y install chromium-browser
-#install visual studio code
-#curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-#sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-#sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-#sudo apt-get update
-#sudo apt-get install -y code
 
 #Code Editors
 sudo snap install atom --classic
