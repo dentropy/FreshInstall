fa233436071fd99e6384bf77a16e0cb96f2f5be2
July312018Ubuntu18.04 <paulmullins@protonmail.com>
2018-08-31 20:20:40 -0400

    Fixing bugs in Mega and GoogleChrome installs

--- DownloadAndInstall/GoogleChrome.sh
+++ DownloadAndInstall/GoogleChrome.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+curl -o https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #Install google chrome
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -y -f install
